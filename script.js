6cfe46c77517b7d5dca5ff70f2fd480efd0524d1
Ubevalohalo <val.vvvalentine.04@gmail.com>
2025-08-22 11:46:11 -0500

    uodates

--- script.js
+++ script.js
@@ -1,22 +1,93 @@
 /*NOTES:
-- I want to make a function that underlines the page user is currently on for the nav
 - I also want to make a function to have the page top headers type out with a typewriter effect
-- generate the same footer for each page, same for nav
 - switch css elements, light mode and dark mode
+- have each section of the pages fade in and out as the user scrolls
 */
-// ---code for hamburger menu ---
-    const hamburgerMenu = document.querySelector('.hamburger-menu');
-    const nav = document.querySelector('nav');
 
-    if (hamburgerMenu && nav) {
-        hamburgerMenu.addEventListener('click', () => {
-            nav.classList.toggle('active');
-        });
-        // Close the menu when a link is clicked
-        document.querySelectorAll('.nav-links a').forEach(link => {
-            link.addEventListener('click', () => {
-                nav.classList.remove('active');
-            });
-        });
+//automate the nav
+function createNavBar() {
+            const navBarHTML = `
+                <nav>
+                    <div id="viewModes">
+                        <label class="switch">
+                            <input type="checkbox">
+                            <span class="slider round"></span>
+                        </label>
+                    </div>
+                    <div class="hamburger-menu">
+                        <span class="bar"></span>
+                        <span class="bar"></span>
+                        <span class="bar"></span>
+                    </div>
+                    <div id="pages" class="nav-links">
+                        <ul id="navList">
+                            <li><a href="index.html">Start</a></li>
+                            <li><a href="about.html" id='aboutPage'>About</a></li>
+                            <li><a href="projects.html" id='projectsPage'>Projects</a></li>
+                        </ul>
+                    </div>
+                    <div id="icons">
+                        <a href="mailto:isabellaepena04@Gmail.com">
+                            <img src="emailIconDarkMode.png">
+                        </a>
+                        <a href="http://www.linkedin.com/in/isabellaepena" target="_blank">
+                            <img src="linkedInIconDarkMode.png">
+                        </a>
+                        <a href="resume.html">
+                            <img src="resumeIconDarkMode.png">
+                        </a>
+                    </div>
+                </nav>
+            `;
+    document.body.insertAdjacentHTML('afterbegin', navBarHTML);        
+};
+//automate the footer
+function createFooter() {
+            const footerHTML = `
+                <footer>
+                    <h2>Let's Connect!</h2>
+                    <div id="connections">
+                        <div id="email">
+                            <a href="mailto:isabellaepena04@Gmail.com">
+                                <img src="emailIconDarkMode.png">
+                                <h3>Email</h3>
+                            </a>
+                        </div>
+                        <div id="linkedIn">
+                            <a href="http://www.linkedin.com/in/isabellaepena" target="_blank">
+                                <img src="linkedInIconDarkMode.png">
+                                <h3>LinkedIn</h3>
+                                </a>
+                        </div>
+                        <div id="resume">
+                            <a href="resume.html">
+                                <img src="resumeIconDarkMode.png">
+                                <h3>View Resume</h3>
+                            </a>
+                        </div>
+                    </div>
+                </footer>
+            `;
+    document.body.insertAdjacentHTML('beforeend', footerHTML);
+};
+
+
+//underline nav item page that user is currently on
+function underlineCurrentPage (){
+    if ( document.URL.includes('about.html') ) {
+        document.getElementById('aboutPage').style.textDecoration = "underline";
+        document.getElementById('aboutPage').style.fontWeight = "bold";
     }
-});
+    else if ( document.URL.includes('projects.html') ) {
+        document.getElementById('projectsPage').style.textDecoration = "underline";
+        document.getElementById('projectsPage').style.fontWeight = "bold";
+    }
+    else {
+        return
+    };
+}
+window.onload = function() {
+    createNavBar();
+    createFooter();
+    underlineCurrentPage();
+}
