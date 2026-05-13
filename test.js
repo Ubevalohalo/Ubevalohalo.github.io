c2342421b3b73ec333199ba0c0ca93b04f90e1ca
Valentine <161781094+Ubevalohalo@users.noreply.github.com>
2026-05-13 11:34:42 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 // CREATE NAVBAR
 function createNavBar() {
 
-    const navBarHTML = `
+    const navContainer = document.getElementById("navContainer");
+
+    navContainer.innerHTML = `
         <nav>
 
             <div class="hamburger-menu">
@@ -10,7 +12,7 @@ function createNavBar() {
                 <span class="bar"></span>
             </div>
 
-            <div id="pages" class="nav-links">
+            <div class="nav-links">
                 <ul id="navList">
                     <li><a href="index.html">Start</a></li>
                     <li><a href="about.html">About</a></li>
@@ -18,79 +20,33 @@ function createNavBar() {
                 </ul>
             </div>
 
-            <div id="icons">
-                <a href="mailto:isabellaepena04@gmail.com">
-                    <img src="emailIconDarkMode.png" alt="Email Icon">
-                </a>
-
-                <a href="https://www.linkedin.com" target="_blank">
-                    <img src="linkedInIconDarkMode.png" alt="LinkedIn Icon">
-                </a>
-
-                <a href="resume.html">
-                    <img src="resumeIconDarkMode.png" alt="Resume Icon">
-                </a>
-            </div>
-
         </nav>
     `;
-
-    // INSERT NAVBAR INTO SPECIFIC CONTAINER
-    document
-        .getElementById("navContainer")
-        .insertAdjacentHTML("beforeend", navBarHTML);
 }
 
 
-
-/* =========================
-   HAMBURGER FUNCTIONALITY
-========================= */
-
+// HAMBURGER MENU
 function setupHamburgerMenu() {
 
     const hamburger = document.querySelector(".hamburger-menu");
-
     const navLinks = document.querySelector(".nav-links");
 
-    const navItems = document.querySelectorAll("#navList a");
-
-
-    // OPEN/CLOSE MENU
     hamburger.addEventListener("click", () => {
-
         hamburger.classList.toggle("active");
-
         navLinks.classList.toggle("active");
-
     });
 
-
-    // CLOSE MENU WHEN LINK CLICKED
-    navItems.forEach(link => {
-
+    document.querySelectorAll("#navList a").forEach(link => {
         link.addEventListener("click", () => {
-
             hamburger.classList.remove("active");
-
             navLinks.classList.remove("active");
-
         });
-
     });
-
 }
 
 
-
-/* =========================
-   PAGE LOAD
-========================= */
-
-window.onload = function() {
-
+// INIT
+window.onload = function () {
     createNavBar();
-
     setupHamburgerMenu();
-
-}
+};
