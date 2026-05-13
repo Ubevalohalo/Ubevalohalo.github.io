1a15de124cd895fee61e3a8cd1cecaa4379f7769
Valentine <161781094+Ubevalohalo@users.noreply.github.com>
2026-05-13 12:55:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,24 +3,19 @@
 function createNavBar() {
             const navBarHTML = `
                 <nav>
-                    <div id="viewModes">
-                        <label class="switch">
-                            <input type="checkbox">
-                            <span class="slider round"></span>
-                        </label>
-                    </div>
-                    <div class="hamburger-menu">
-                        <span class="bar"></span>
-                        <span class="bar"></span>
-                        <span class="bar"></span>
-                    </div>
-                    <div id="pages" class="nav-links">
-                        <ul id="navList">
-                            <li><a href="index.html">Start</a></li>
-                            <li><a href="about.html" id='aboutPage'>About</a></li>
-                            <li><a href="projects.html" id='projectsPage'>Projects</a></li>
-                        </ul>
-                    </div>
+   <div class="hamburger">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
+
+            <div class="nav-links">
+                <ul id="navList">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Projects</a></li>
+                </ul>
+            </div>
                     <div id="icons">
                         <a href="mailto:isabellaepena04@Gmail.com">
                             <img src="emailIconDarkMode.png">
