54ed4a3d58ec722f376be4baad04f2b9b0e72cfd
Valentine <161781094+Ubevalohalo@users.noreply.github.com>
2026-05-13 11:19:12 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,35 +1,33 @@
-//automate the nav
-function createNavBar() {
-            const navBarHTML = `
-                <nav>
-                    <div class="hamburger-menu">
-                        <span class="bar"></span>
-                        <span class="bar"></span>
-                        <span class="bar"></span>
-                    </div>
-                    <div id="pages" class="nav-links">
-                        <ul id="navList">
-                            <li><a href="index.html">Start</a></li>
-                            <li><a href="about.html" id='aboutPage'>About</a></li>
-                            <li><a href="projects.html" id='projectsPage'>Projects</a></li>
-                        </ul>
-                    </div>
-                    <div id="icons">
-                        <a href="mailto:isabellaepena04@Gmail.com">
-                            <img src="emailIconDarkMode.png">
-                        </a>
-                        <a href="http://www.linkedin.com/in/isabellaepena" target="_blank">
-                            <img src="linkedInIconDarkMode.png">
-                        </a>
-                        <a href="resume.html">
-                            <img src="resumeIconDarkMode.png">
-                        </a>
-                    </div>
-                </nav>
-            `;
-            
-            
-            
-window.onload = function() {
-    createNavBar();
-}
+const navBarHTML = `
+    <nav>
+
+        <div class="hamburger-menu">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </div>
+
+        <div id="pages" class="nav-links">
+            <ul id="navList">
+                <li><a href="index.html">Start</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="projects.html">Projects</a></li>
+            </ul>
+        </div>
+
+        <div id="icons">
+            <a href="#">
+                <img src="https://cdn-icons-png.flaticon.com/512/732/732200.png">
+            </a>
+
+            <a href="#">
+                <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png">
+            </a>
+
+            <a href="#">
+                <img src="https://cdn-icons-png.flaticon.com/512/2991/2991148.png">
+            </a>
+        </div>
+
+    </nav>
+`;
